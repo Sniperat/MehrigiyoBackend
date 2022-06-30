528dd81339a8235c44e1488954da99a5acbb5b82
Sniperat <muhammadalixonakbarov2000@gmail.com>
2022-06-30 17:49:22 +0500

    little change

--- specialist/urls.py
+++ specialist/urls.py
@@ -4,7 +4,7 @@ from .views import (DoctorsView, TypeDoctorView, GetDoctorsWithType, GetSingleDo
 urlpatterns = [
     path('types/', TypeDoctorView.as_view()),
     path('types/<int:pk>/', GetDoctorsWithType.as_view()),
-    path('doctors/', DoctorsView.as_view()),
+    path('doctors/', DoctorsView.as_view({'list': 'get'})),
     path('advertising/', AdvertisingView.as_view()),
     path('doctors/<int:pk>/', GetSingleDoctor.as_view()),
     path('advice/<int:pk>/', AdviceView.as_view()),
