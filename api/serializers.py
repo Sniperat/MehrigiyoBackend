25bcd95d113cd652f4bfde6927186cd95147d638
Sniperat <muhammadalixonakbarov2000@gmail.com>
2022-07-05 11:30:18 +0500

    little changes

--- api/serializers.py
+++ api/serializers.py
@@ -3,42 +3,36 @@ from account.models import UserModel, SmsCode, SmsAttempt, CountyModel, RegionMo
 
 
 class UserModelAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = UserModel
         fields = '__all__'
 
 
 class SmsCodeAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = SmsCode
         fields = '__all__'
 
 
 class SmsAttemptAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = SmsAttempt
         fields = '__all__'
 
 
 class CountryModelAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = CountyModel
         fields = '__all__'
 
 
 class RegionModelAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = RegionModel
         fields = '__all__'
 
 
 class DeliveryAddressAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = DeliveryAddress
         fields = '__all__'
@@ -48,32 +42,27 @@ from chat.models import Message, ChatRoom
 
 
 class MessageAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Message
         fields = '__all__'
 
 
 class ChatRoomAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = ChatRoom
         fields = '__all__'
-
 
 
 from comment.models import CommentDoctor, CommentMedicine
 
 
 class CommentDoctorAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = CommentDoctor
         fields = '__all__'
 
 
 class CommentMedicineAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = CommentMedicine
         fields = '__all__'
@@ -83,7 +72,6 @@ from news.models import NewsModel
 
 
 class NewsModelAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = NewsModel
         fields = '__all__'
@@ -93,14 +81,12 @@ from paymeuz.models import PaymeTransactionModel, Card
 
 
 class PaymeTransactionModelAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = PaymeTransactionModel
         fields = '__all__'
 
 
 class CardAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Card
         fields = '__all__'
@@ -110,41 +96,37 @@ from shop.models import PicturesMedicine, TypeMedicine, Medicine, CartModel, Del
 
 
 class PicturesMedicineAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = PicturesMedicine
         fields = '__all__'
 
 
 class TypeMedicineAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = TypeMedicine
         fields = '__all__'
 
 
 class MedicineAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Medicine
         fields = '__all__'
 
 
 class CartModelAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = CartModel
         fields = '__all__'
 
 
 class DeliveryManAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = DeliveryMan
         fields = '__all__'
 
 
 class OrderModelAdminSerializer(serializers.ModelSerializer):
+    user = UserModelAdminSerializer()
 
     class Meta:
         model = OrderModel
@@ -155,30 +137,24 @@ from specialist.models import TypeDoctor, Doctor, RateDoctor, AdviceTime
 
 
 class TypeDoctorAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = TypeDoctor
         fields = '__all__'
 
 
 class DoctorAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Doctor
         fields = '__all__'
 
 
 class RateDoctorAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = RateDoctor
         fields = '__all__'
 
 
 class AdviceTimeAdminSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = AdviceTime
         fields = '__all__'
-
-
