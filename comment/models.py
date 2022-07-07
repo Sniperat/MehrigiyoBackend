1fa6ca68b9342e4cf1155649afd91b21311ab174
Sniperat <muhammadalixonakbarov2000@gmail.com>
2022-07-07 14:22:59 +0500

    little changes

--- comment/models.py
+++ comment/models.py
@@ -6,7 +6,7 @@ from specialist.models import Doctor
 
 
 class CommentMedicine(models.Model):
-    medicine = models.ForeignKey(Medicine, on_delete=models.RESTRICT)
+    medicine = models.ForeignKey(Medicine, on_delete=models.RESTRICT, related_name='comments_med')
     user = models.ForeignKey(UserModel, on_delete=models.RESTRICT)
     text = models.TextField()
     rate = models.SmallIntegerField(default=1)
@@ -15,7 +15,7 @@ class CommentMedicine(models.Model):
 
 class CommentDoctor(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.RESTRICT, related_name='comments_doc')
-    user = models.ForeignKey(UserModel, on_delete=models.RESTRICT, related_name='comments_med')
+    user = models.ForeignKey(UserModel, on_delete=models.RESTRICT)
     text = models.TextField(null=True, blank=True)
     rate = models.SmallIntegerField(default=1)
     created_at = models.DateTimeField(auto_now=True, null=True)
